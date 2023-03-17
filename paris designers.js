09c6f1f4be20f1a2ac92f7970e9a6492963e4590
Dithurka  Krishnamoorthy <118276148+D-Krish@users.noreply.github.com>
2023-03-17 22:22:49 +0530

    project 2
    project 2

---
+++ paris designers.js
@@ -0,0 +1,7 @@
+function locationmap(){
+var mapoflocation={
+    center:new google.maps.Latlng(90,-90),
+    zoom:6,};
+
+var map=new google.maps.Map(document.getElementById("googlemapaddres"),mapoflocation);
+}
